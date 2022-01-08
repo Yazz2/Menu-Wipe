039dda1f41b00a559001de2fc47e85873760edf8
Yazz <88659966+Yazz2@users.noreply.github.com>
2022-01-08 13:43:26 +0100

    Update sv_wipe.lua

--- wipe/sv_wipe.lua
+++ wipe/sv_wipe.lua
@@ -4,62 +4,35 @@ TriggerEvent('esx:getSharedObject', function(obj)
     ESX = obj 
 end)
 
-RegisterNetEvent("sWipe:WipePlayer")
-AddEventHandler("sWipe:WipePlayer", function(player)
-    WipePlayer(source, player)
+RegisterNetEvent("yWipe:Wipe")
+AddEventHandler("yWipe:Wipe", function(joueur)
+    Wipe(source, joueur)
 end)
 
-function WipePlayer(id, target, source)
+function Wipe(id, target)
     local xPlayer = ESX.GetPlayerFromId(target)
-    local xPlayers = ESX.GetPlayerFromId(source)
-    local steam = xPlayer.getIdentifier()
+    local steam = xPlayer.getIdentifier(target)
 
-    DropPlayer(target, "Vous vous êtes fait wipe !")
+
+    DropPlayer(target, "Vous avez était wipe de yzBase Bonne chance !")
+-- Rajouter les tables que vous voulez au dessus de celle deja mise exemple = "DELETE FROM "nom de votre table" WHERE identifier = @id;" 
+-- Pour savoir quelle table mettre regardez dans votre BDD
 
     MySQL.Async.execute([[ 
-		DELETE FROM billing WHERE identifier = @wipeID;
-		DELETE FROM billing WHERE sender = @wipeID;
-		DELETE FROM open_car WHERE identifier = @wipeID;
-		DELETE FROM owned_vehicles WHERE owner = @wipeID;
-        DELETE FROM user_accounts WHERE identifier = @wipeID;
-        DELETE FROM user_accessories WHERE identifier = @wipeID;
-		DELETE FROM phone_users_contacts WHERE identifier = @wipeID;
-		DELETE FROM user_inventory WHERE identifier = @wipeID;
-        DELETE FROM user_licenses WHERE owner = @wipeID;
-        DELETE FROM user_tenue WHERE identifier = @wipeID;
- 		DELETE FROM users WHERE identifier = @wipeID;	]], {
-		['@wipeID'] = steam,
-    }, function(rowsChanged)
-        print("^5Wipe effectuer ! SteamID :"..steam.."^0")
-        TriggerClientEvent('esx:showNotification', id, "~g~Joueur Wipe avec succés")
-    end)
-    --DELETE FROM owned_properties WHERE owner = @wipeID;
-    --DELETE FROM playerstattoos WHERE identifier = @wipeID;
-    --DELETE FROM owned_boats WHERE owner = @wipeID;
-
-    -- ADD YOUR TABLE / DELETE WHAT YOU DON'T HAVE 
-end
-
-function sendToDiscordWithSpecialURL(name,message,color,url)
-    local DiscordWebHook = url
-	local embeds = {
-		{
-			["title"]=message,
-			["type"]="rich",
-			["color"] =color,
-		}
 		
-	}
-    if message == nil or message == '' then return FALSE end
-    PerformHttpRequest(WipeLogs, function(err, text, headers) end, 'POST', json.encode({ username = name,embeds = embeds}), { ['Content-Type'] = 'application/json' })
+        DELETE FROM users WHERE identifier = @id;	]], {
+            ['@id'] = steam,
+            
+        }, function(rowsChanged)
+
+        print("^1Wipe effectuer ! SteamID :"..steam.."^0")
+        TriggerClientEvent('esx:showNotification', id, "Player wipe successfully performed")
+    end)
+
 end
 
 
-RegisterNetEvent('Wipe:MenuWipe')
-AddEventHandler('Wipe:MenuWipe', function()
-	local _src = source
-    local xPlayer = ESX.GetPlayerFromId(target)
-    local xPlayers = ESX.GetPlayerFromId(source)
-	sendToDiscordWithSpecialURL(target,"Menu Wipe ",xPlayers.getName().." à utilisé le menu wipe", 0, WipeLogs)
 
-end)
+
+
+
